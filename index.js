f119197f4a34d00b6813b54068e271a749a663e3
ndhanh96 <ndhanh96@gmail.com>
2021-03-05 10:56:57 +0700

    using morgan ++ done exercise

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require('cors');
 const app = express();
 
+morgan.token("content", (req, res) => {
+  return req.body.number && req.body.name
+    ? `{"name": ${req.body.name}, "number": ${req.body.number}}`
+    : "- -";
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :content"
+  )
+);
 app.use(express.json());
+app.use(cors());
 
 let persons = [
   {
@@ -44,13 +58,15 @@ app.get("/api/persons/:id", (req, res) => {
 
 app.delete("/api/delete/:id", (req, res) => {
   const id = Number(req.params.id);
-  const person = persons.filter(p => p.id !== id);
+  const person = persons.filter((p) => p.id !== id);
   console.log(persons);
-  console.log('check',persons.find(p => p.id === id));
+  console.log(
+    "check",
+    persons.find((p) => p.id === id)
+  );
 
-  if(persons.find(p => p.id === id) == undefined) {
+  if (persons.find((p) => p.id === id) == undefined) {
     return res.status(404).end();
-    
   }
 
   if (person) {
@@ -59,34 +75,34 @@ app.delete("/api/delete/:id", (req, res) => {
   }
 });
 
-app.post('/api/persons', (req, res) => {
-    const maxnumber = Math.max(...persons.map(p => p.id))
-    let randomnumber = Math.floor(Math.random() * Math.floor(10)) + maxnumber;
-    if(persons.find(p => p.id === randomnumber)) {
-      console.log(randomnumber);
-      res.status(404).end();
-      return;
-    }
+app.post("/api/persons", (req, res) => {
+  const maxnumber = Math.max(...persons.map((p) => p.id));
+  let randomnumber = Math.floor(Math.random() * Math.floor(10)) + maxnumber;
+  if (persons.find((p) => p.id === randomnumber)) {
+    console.log(randomnumber);
+    res.status(404).end();
+    return;
+  }
 
-    body = req.body;
+  body = req.body;
 
-    if(!body.name || persons.find(p => p.name === body.name)) {
-      return res.status(400).json({
-        error: 'name is required and must be unique'
-      });
-    }
+  if (!body.name || persons.find((p) => p.name === body.name)) {
+    return res.status(400).json({
+      error: "name is required and must be unique",
+    });
+  }
 
-    if(!body.number) {
-      return res.status(400).json({
-        error: 'number is required'
-      });
-    }
+  if (!body.number) {
+    return res.status(400).json({
+      error: "number is required",
+    });
+  }
 
-    persons.push({id:randomnumber, name: body.name, number: body.number});
-    
-    console.log(persons);
-    res.json(persons);
-    res.end();
+  persons.push({ id: randomnumber, name: body.name, number: body.number });
+
+  console.log(persons);
+  res.json(persons);
+  res.end();
 });
 
 app.get("/info", (req, res) => {
@@ -95,7 +111,13 @@ app.get("/info", (req, res) => {
   res.end();
 });
 
-const PORT = 3001;
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is listening at ${PORT}`);
 });
